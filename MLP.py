137b5bea722237bcfc311ed342a1ef5766250d12
Vlad <69718459+VladFearo@users.noreply.github.com>
2023-01-16 16:10:19 +0200

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -6,6 +6,8 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 
+
+# Start time counter
 start_time = time.perf_counter()
 
 # Load the digits dataset
@@ -14,6 +16,7 @@ n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.5, shuffle=False)
+# Creating MLP object
 mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=50, alpha=1e-4,
                     solver='lbfgs',  tol=1e-4, random_state=1,
                     learning_rate_init=1e-3, verbose=True)
@@ -28,21 +31,19 @@ print(f"Runtime: {run_time*1000:.2f} ms")
 accuracy = mlp.score(X_test, y_test)
 print("Test set accuracy: {:.2f}".format(accuracy))
 predicted = mlp.predict(X_test)
+# Showing 4 pictures of numbers with their prediction
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, prediction in zip(axes, X_test, predicted):
     ax.set_axis_off()
     image = image.reshape(8, 8)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
-
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
-
+ # Creating and printing out a report of the model success and accuracy on the testing samples
 print(
     f"Classification report for classifier {mlp}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
+# Creating and showing the confusion matrix
 disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
 disp.figure_.suptitle("Confusion Matrix")
 print(f"Confusion matrix:\n{disp.confusion_matrix}")
